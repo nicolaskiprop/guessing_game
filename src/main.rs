04cffc4a3f271c1dcd88afe7a3b3d8ea9b288d18
nicolskiprop <yegonicholas@gmail.com>
2022-04-28 15:49:16 +0300

    error handled

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io;
-
+use std::cmp::Ordering;
 use rand::Rng;
 
 
@@ -12,12 +12,32 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("please input your guess.");
+    loop{
+        println!("please input your guess.");
+    
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    
+    
+    
+        println!("you guessed: {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => { 
+                 println!("You win!");
+                 break;
+            }
 
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("you guessed: {}", guess);
+        }
+    }
 }
